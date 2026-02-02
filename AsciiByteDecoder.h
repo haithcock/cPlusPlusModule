eba58c46cf6176d5c95207f9ba597d0282a5dcd0
haithcock <dakota.haith1997@gmail.com>
2026-02-02 13:14:31 -0500

    wrote more sauce but for AsciiByteDecoder.h

--- AsciiByteDecoder.h
+++ AsciiByteDecoder.h
@@ -6,3 +6,8 @@ protected:
     const char* decoderName() const override;
     std::string decodeByte(std::uint8_t b) const override;
 };
+
+//class AsciiByteDecoder inherits from the ByteDecoder class I made earlier.
+//The protected 'const char* decoderName() const override;
+//    std::string decodeByte(std::uint8_t b) const override;'
+// part tells my compiler this bad boy knows how to decode some bytes but we dont have our logic yet.
